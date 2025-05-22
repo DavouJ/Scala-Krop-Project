e4257341ea3ec0f8fe958df12a59a27c15e6bb14
DavouJ <96893264+DavouJ@users.noreply.github.com>
2025-05-22 19:40:02 +0100

    Second Commit

---
+++ backend/src/scalakropproject/conf/Context.scala
@@ -0,0 +1,26 @@
+package scalakropproject.conf
+
+import cats.effect.IO
+import cats.effect.Resource
+import krop.Mode
+import krop.sqlite.Sqlite
+import krop.sqlite.Transactor
+
+// This contains all the dependencies your application needs to run
+final case class Context(xa: Transactor)
+object Context {
+  // The development mode context
+  val development: Resource[IO, Context] =
+    Sqlite.create("./scalakropproject-development.sqlite3").map(xa => Context(xa))
+
+  // The production mode context
+  val production: Resource[IO, Context] =
+    Sqlite.create("./scalakropproject-production.sqlite3").map(xa => Context(xa))
+
+  // The context for the current mode
+  val current: Resource[IO, Context] =
+    Mode.mode match {
+      case Mode.Development => development
+      case Mode.Production  => production
+    }
+}
